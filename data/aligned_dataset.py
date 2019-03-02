fixing opencv normalization

data/aligned_dataset.py
@@ -32,7 +32,7 @@ class AlignedDataset(BaseDataset):
         self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
         self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
 
-    def __getitem_old__(self, index):
+    def __getitem__old(self, index):
         """Return a data point and its metadata information.
 
         Parameters:
@@ -51,7 +51,7 @@ class AlignedDataset(BaseDataset):
         #AB = Image.fromarray(float_img)
         #AB = Image.open(AB_path).convert('RGB')
 
-        # img = Image.open(AB_path).convert('RGB');
+        AB = Image.open(AB_path).convert('RGB');
         # print(img.getextrema());
         nparr = np.array(AB);
         max_val = np.amax(nparr);
@@ -88,19 +88,31 @@ class AlignedDataset(BaseDataset):
         # read a image given a random integer index
         AB_path = self.AB_paths[index]
         cv2_img = cv2.imread(AB_path, cv2.IMREAD_COLOR | cv2.IMREAD_ANYDEPTH);
+        #cv2_img = cv2.imread(AB_path, cv2.IMREAD_COLOR);
 
-        print(np.amax(cv2_img))
+        depth_div_factor = 65535.0 if cv2_img.dtype == np.uint16 else 255.0
         cv2_img = cv2_img.astype('float32')
-        print(np.amax(cv2_img))
+        cv2_img = cv2_img[..., ::-1] / depth_div_factor
+        # rgb = B_numpy_conv[..., ::-1]
         ht, wd, channels = cv2_img.shape
         half_wd = int(wd/2);
 
         A_numpy = cv2_img[:, 0:half_wd, :]
         B_numpy = cv2_img[:, half_wd:wd, :]
 
+        # A_numpy = np.copy(A_numpy[:, ::-1, :])
+        # B_numpy = np.copy(B_numpy[:, ::-1, :])
+
+        B_r = B_numpy[:,:,2]
+        B_g = B_numpy[:,:,1]
+        B_b = B_numpy[:,:,0]
+
+        print("Opening images as 16bit : ", self.opt.is_16_bit);
+        print("Max val of A, B : ", np.amax(A_numpy), np.amax(B_numpy));
+
         #float_img = cv2_img.astype(float)
         #AB = Image.fromarray(float_img)
-        AB = Image.open(AB_path).convert('RGB')
+        # AB = Image.open(AB_path).convert('RGB')
 
         # img = Image.open(AB_path).convert('RGB');
         # print(img.getextrema());
@@ -109,15 +121,15 @@ class AlignedDataset(BaseDataset):
         #
         # split AB image into A and B
 
-        w, h = AB.size
-        w2 = int(w / 2)
-        A = AB.crop((0, 0, w2, h))
+        # w, h = AB.size
+        # w2 = int(w / 2)
+        # A = AB.crop((0, 0, w2, h))
         # B = AB.crop((w2, 0, w, h))
 
         # apply the same transform to both A and B
-        transform_params = get_params(self.opt, A.size)
-        A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
-        B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
+        # transform_params = get_params(self.opt, A.size)
+        # A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
+        # B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
 
         A_transform = get_transform_cv2(self.opt)
         B_transform = get_transform_cv2(self.opt)
@@ -139,9 +151,19 @@ class AlignedDataset(BaseDataset):
         # B_numpy_conv = B_numpy_conv.transpose((1, 2, 0))
         #
         #
-        # parts = AB_path.rsplit('.', 1);
-        # name = parts[0]
-        # ext = parts[1]
+
+        dirs = AB_path.rsplit('\\', 1);
+        dir = dirs[0]
+        full_name = dirs[1]
+
+        parts = full_name.rsplit('.', 1);
+        name = parts[0]
+        ext = parts[1]
+
+        # cv2.imwrite(dir + '/preview/' + name + '_B_r.' + ext, B_r.astype(np.uint16))
+        # cv2.imwrite(dir + '/preview/' + name + '_B_g.' + ext, B_g.astype(np.uint16))
+        # cv2.imwrite(dir + '/preview/' + name + '_B_b.' + ext, B_b.astype(np.uint16))
+
         # cv2.imwrite(name + '_A.' + ext, A_numpy_conv)
         # cv2.imwrite(name + '_B.' + ext, B_numpy_conv)
         #